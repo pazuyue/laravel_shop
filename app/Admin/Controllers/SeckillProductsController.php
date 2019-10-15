d52060799c4a7dbc3dcc8e9dddf62e47ec74db19
yueguang <yueguang@qingmutec.com>
2019-10-15 14:41:52 +0800

    众筹商品功能

---
+++ app/Admin/Controllers/SeckillProductsController.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/10/15
+ * Time: 10:55
+ */
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+
+class SeckillProductsController extends Controller
+{
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header(Product::$typeMap[$this->getProductType()].'列表')
+            ->body($this->grid());
+    }
+
+    public function getProductType()
+    {
+        return Product::TYPE_SECKILL;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('seckill.start_at', '开始时间');
+        $grid->column('seckill.end_at', '结束时间');
+        $grid->sold_count('销量');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 秒杀相关字段
+        $form->datetime('seckill.start_at', '秒杀开始时间')->rules('required|date');
+        $form->datetime('seckill.end_at', '秒杀结束时间')->rules('required|date');
+    }
+
+    protected function grid()
+    {
+        $grid = new Grid(new Product());
+        // 筛选出当前类型的商品，默认 ID 倒序排序
+        $grid->model()->where('type', $this->getProductType())->orderBy('id', 'desc');
+        // 调用自定义方法
+        $this->customGrid($grid);
+        $grid->actions(function ($actions) {
+            $actions->disableView();
+            $actions->disableDelete();
+        });
+        $grid->tools(function ($tools) {
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+        return $grid;
+    }
+
+}
--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,17 +13,20 @@ Route::group([
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
 
+    //商品
     $router->get('products', 'ProductsController@index');
     $router->get('products/create', 'ProductsController@create');
     $router->post('products', 'ProductsController@store');
     $router->get('products/{id}/edit', 'ProductsController@edit');
     $router->put('products/{id}', 'ProductsController@update');
 
+    //订单
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show');
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
     $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
 
+    //优惠券
     $router->get('coupon_codes', 'CouponCodesController@index');
     $router->post('coupon_codes', 'CouponCodesController@store');
     $router->get('coupon_codes/create', 'CouponCodesController@create');
@@ -31,6 +34,7 @@ Route::group([
     $router->put('coupon_codes/{id}', 'CouponCodesController@update');
     $router->delete('coupon_codes/{id}', 'CouponCodesController@destroy');
 
+    //类目
     $router->get('categories', 'CategoriesController@index');
     $router->get('categories/create', 'CategoriesController@create');
     $router->get('categories/{id}/edit', 'CategoriesController@edit');
@@ -38,4 +42,18 @@ Route::group([
     $router->put('categories/{id}', 'CategoriesController@update');
     $router->delete('categories/{id}', 'CategoriesController@destroy');
     $router->get('api/categories', 'CategoriesController@apiIndex');
+
+    //众筹
+    $router->get('crowdfunding_products', 'CrowdfundingProductsController@index');
+    $router->get('crowdfunding_products/create', 'CrowdfundingProductsController@create');
+    $router->post('crowdfunding_products', 'CrowdfundingProductsController@store');
+    $router->get('crowdfunding_products/{id}/edit', 'CrowdfundingProductsController@edit');
+    $router->put('crowdfunding_products/{id}', 'CrowdfundingProductsController@update');
+
+    //秒杀
+    $router->get('seckill_products', 'SeckillProductsController@index');
+    $router->get('seckill_products/create', 'SeckillProductsController@create');
+    $router->post('seckill_products', 'SeckillProductsController@store');
+    $router->get('seckill_products/{id}/edit', 'SeckillProductsController@edit');
+    $router->put('seckill_products/{id}', 'SeckillProductsController@update');
 });
