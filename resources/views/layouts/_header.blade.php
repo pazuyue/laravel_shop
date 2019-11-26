a11d8e993151cd326f76b2e75b05003bc8bfeb7c
yueguang <yueguang@qingmutec.com>
2019-11-26 15:40:59 +0800

    获取用户头衔

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,7 +37,7 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            <img src="http://blog.yikaiba.com//Public/Uploads/2018-03-02/5a98a8230061b.jpg" class="img-responsive img-circle" width="30px" height="30px">
             {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
