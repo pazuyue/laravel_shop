9aa766dbd1a43b1679e12e424deb54d95a047688
yueguang <yueguang@qingmutec.com>
2019-11-26 19:38:57 +0800

    预定商品 模块添加

--- resources/views/admin/advance_orders/show.blade.php
+++ resources/views/admin/advance_orders/show.blade.php
@@ -56,24 +56,6 @@
                             <form action="{{ route('admin.advance_orders.pay', [$order->id]) }}" method="post" class="form-inline">
                                 <!-- 别忘了 csrf token 字段 -->
                                 {{ csrf_field() }}
-                                <div class="form-group {{ $errors->has('express_company') ? 'has-error' : '' }}">
-                                    <label for="express_company" class="control-label">物流公司</label>
-                                    <input type="text" id="express_company" name="express_company" value="" class="form-control" placeholder="输入物流公司">
-                                    @if($errors->has('express_company'))
-                                        @foreach($errors->get('express_company') as $msg)
-                                            <span class="help-block">{{ $msg }}</span>
-                                        @endforeach
-                                    @endif
-                                </div>
-                                <div class="form-group {{ $errors->has('express_no') ? 'has-error' : '' }}">
-                                    <label for="express_no" class="control-label">物流单号</label>
-                                    <input type="text" id="express_no" name="express_no" value="" class="form-control" placeholder="输入物流单号">
-                                    @if($errors->has('express_no'))
-                                        @foreach($errors->get('express_no') as $msg)
-                                            <span class="help-block">{{ $msg }}</span>
-                                        @endforeach
-                                    @endif
-                                </div>
                                 <button type="submit" class="btn btn-success" id="ship-btn">支付</button>
                             </form>
                         </td>
